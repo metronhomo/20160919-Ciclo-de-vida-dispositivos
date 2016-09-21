280430451a5b2851524e71d646a2f82c7528a8b0
JM <ecojmbarrera@gmail.com>
2016-09-21 12:06:03 -0500

    Abiertas
    abiertas

--- reporteAbiertas.R
+++ reporteAbiertas.R
@@ -20,7 +20,6 @@ datos$Total <- factor(1,levels = 1,labels = "Total")
 
 datos[is.na(datos$P15_B_CELULAR_Hablar_por_tel_fono),"P15_B_CELULAR_Hablar_por_tel_fono"]<-0
 
-
 ##########################################################################################
 
 # Reporte
@@ -50,19 +49,24 @@ table(datos$P18_A_COMPU_Otra__Especificar)
 
 # .-------------------------------------------
 
-exportarAbiertas(xpa = datos,xpb = "P16_CELULAR_OTRO",xpc = "./abiertas/P16_Celular")
-exportarAbiertas(xpa = datos,xpb = "P16_COMPU_OTRO",xpc = "./abiertas/P16_Compu")
-exportarAbiertas(xpa = datos,xpb = c("P16_CELULAR_OTRO","P16_COMPU_OTRO"),xpc = "./abiertas/P16_Compu")
+exportarAbiertas(xpa = datos,xpb = "P16_CELULAR_OTRO",xpc = "./abiertas/P16_CELULAR_OTRO.csv")
 # table(datos$P16_CELULAR_OTRO)
 # table(datos$P16_COMPU_OTRO)
 # 
-table(datos$P16_J_CELULAR_OTRO)
+# table(datos$P16_J_CELULAR_OTRO)
+exportarAbiertas(xpa = datos,xpb = "P16_J_CELULAR_OTRO",xpc = "./abiertas/P16_J_CELULAR_OTRO.csv")
 
-table(datos$P17_B_CELULAR_OTRO) #Esta tiene catalogo....
-table(datos$P17_E_CELULAR_OTRO) #Esta tiene catalogo....
+# table(datos$P17_B_CELULAR_OTRO) #Esta tiene catalogo....
+exportarAbiertas(xpa = datos,xpb = "P17_B_CELULAR_OTRO",xpc = "./abiertas/P17_B_CELULAR_OTRO.csv")
+# table(datos$P17_E_CELULAR_OTRO) #Esta tiene catalogo....
+exportarAbiertas(xpa = datos,xpb = "P17_E_CELULAR_OTRO",xpc = "./abiertas/P17_E_CELULAR_OTRO.csv")
 
-table(datos$P17_F_CELULAR_OTRO) 
+# table(datos$P17_F_CELULAR_OTRO) 
+exportarAbiertas(xpa = datos,xpb = "P17_F_CELULAR_OTRO",xpc = "./abiertas/P17_F_CELULAR_OTRO.csv")
 
-table(datos$P18_A_CELULAR_OTRO) #Esta tiene catalogo....
+# table(datos$P18_A_CELULAR_OTRO) #Esta tiene catalogo....
+exportarAbiertas(xpa = datos,xpb = "P18_A_CELULAR_OTRO",xpc = "./abiertas/P18_A_CELULAR_OTRO.csv")
+
+# .-------------------------------------------
 
 
